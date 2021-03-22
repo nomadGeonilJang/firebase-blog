3c2a300b48b5d3eb4d28ed8a8abd837f0b1f25a9
nomadGeonilJang <wkdrjsdlf2@gmail.com>
2021-03-22 11:09:44 +0900

    add eslint

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { BrowserRouter } from "react-router-dom";
 import reportWebVitals from './reportWebVitals';
+
+import App from 'App';
+import GlobalStyle from 'components/layout/global.component';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <GlobalStyle/>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById( 'root' )
 );
 
 // If you want to start measuring performance in your app, pass a function
